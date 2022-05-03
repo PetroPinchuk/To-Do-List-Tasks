a812205e16d20caff7059e0867540b64f72daafe
Petro <locator862@gmail.com>
2022-05-03 15:33:32 +0200

    Changed background color of the body

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Layout = ({children}) => {
   return (
-    <div className= 'py-10 bg-gray-900 h-screen'>
+    <div className= 'py-20'>
         {children}
     </div>
   )
