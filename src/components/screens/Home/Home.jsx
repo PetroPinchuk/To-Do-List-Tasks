e5f77b708c3eab73a91658ebbf3d36e781bbf194
Petro <locator862@gmail.com>
2022-05-03 14:31:02 +0200

    modified sorting logic

--- src/components/screens/Home/Home.jsx
+++ src/components/screens/Home/Home.jsx
@@ -2,29 +2,14 @@ import React, { useState } from 'react'
 import CreateTodoField from './create-todo-field/CreateTodoField'
 import TodoItem from './item/TodoItem'
 
-const data = [
-{
-    _id: '1weghc65h',
-    title: 'Finish the essay collaboration',
-    isCompleted: false,
-},
-{
-    _id: '2weghc87h',
-    title: 'Read next chapter of the book',
-    isCompleted: false,
-},
-{
-    _id: '3weghc55h',
-    title: 'Send the finished assignment',
-    isCompleted: false,
-},
-]
-
 
 const Home = () => {
-    const [todos, setTodos] = useState(data)
-
+    const [todos, setTodos] = useState([])
+    
     const changeTodoStatus = id => {
+        todos.sort((a,b) => {
+            return a.sortId - b.sortId
+        })
         const copy = [...todos]
         const current = copy.find(t => t._id === id)
         current.isCompleted = !current.isCompleted
@@ -38,17 +23,22 @@ const Home = () => {
         setTodos([...todos].filter(t => t._id !== id))
     }
 
+    console.log(todos)
+
 
     return(
-        <div className='text-white w-4/5 mx-auto'>
+        <div className='text-white w-4/5 mx-auto '>
             <h1 className='text-2xl font-bold text-center mb-8'>To-Do List & Tasks</h1>
-            <CreateTodoField setTodos={setTodos} />
+            <CreateTodoField 
+                todos={todos} 
+                setTodos={setTodos} />
             {todos.map(todo => (
                 <TodoItem 
                     key={todo._id} 
                     todo={todo}
                     changeTodoStatus={changeTodoStatus}
-                    removeTodo={removeTodo} />
+                    removeTodo={removeTodo}
+                     />
             ))}
         </div>
     )
--- src/components/screens/Home/create-todo-field/CreateTodoField.jsx
+++ src/components/screens/Home/create-todo-field/CreateTodoField.jsx
@@ -1,18 +1,32 @@
 import React, { useState } from 'react'
 
-const CreateTodoField = ({setTodos}) => {
+const CreateTodoField = ({setTodos, todos}) => {
   
   const[title, setTitle] = useState('')
-
-  const addTodo = (title) => {
-    setTodos(prev => [
-    {
-        _id: new Date(),
-        title: title,
-        isCompleted: false
-    }, ...prev,])
+  const [lastSortId, setLastSortId] = useState(0)
+  
+  const addTodo = (title, lastSortId, todos) => {
+    const newId = lastSortId + 1
+    setLastSortId(newId)
+    setTodos(prev => {
+      let newTodo = [
+        ...prev, {
+          _id: Date.now(),
+          title: title,
+          isCompleted: false,
+          sortId: newId,
+      }, ]
+      newTodo.sort((a,b) => {
+        return a.sortId - b.sortId
+      })
+      newTodo.sort((a, b) => {
+        return a.isCompleted - b.isCompleted
+      })
+      return newTodo
+    })
     setTitle('')
 }
+
 
   return (
     <div className='flex items-center justify-between mb-5 rounded-2xl border-zinc-800 border-2 px-5 py-3 w-full mt-5'>
@@ -20,7 +34,7 @@ const CreateTodoField = ({setTodos}) => {
             type="text" 
             onChange={e => setTitle(e.target.value)} 
             value={title}
-            onKeyPress={e => e.key === 'Enter' && addTodo(title)}
+            onKeyPress={e => e.key === 'Enter' && addTodo(title, lastSortId, todos)}
             placeholder='Add a task'
             className='bg-transparent w-full border-none outline-none' />
     </div>
