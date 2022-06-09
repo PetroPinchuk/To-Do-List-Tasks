5bdd3d8a863d7dab76c35157724e1a217ac03c0e
PetroPinchuk <75385552+PetroPinchuk@users.noreply.github.com>
2022-06-09 13:53:16 +0300

    Update Home.jsx

--- src/components/screens/Home/Home.jsx
+++ src/components/screens/Home/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
 
 
     return(
-        <div className='text-white w-4/5 mx-auto '>
+        <div className='text-white w-2/5 mx-auto '>
             <h1 className='text-2xl font-bold text-center mb-8'>To-Do List & Tasks</h1>
             <CreateTodoField 
                 todos={todos} 
